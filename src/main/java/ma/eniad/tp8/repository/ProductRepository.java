975efe5b3c52ed9e38b660a8717c29af8bb7a7ca
yassine4real <yssnabbou2002@gmail.com>
2024-12-29 19:24:42 +0100

    add ProductRepository to repository package for db operation

---
+++ src/main/java/ma/eniad/tp8/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package ma.eniad.tp8.repository;
+
+
+import ma.eniad.tp8.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
